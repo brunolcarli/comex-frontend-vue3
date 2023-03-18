bfec2ef019266328fccc8dc3bef86b7990d18375
Alana Auerbach Carsten <alanacarsten@gmail.com>
2023-03-18 17:10:19 -0300

    Migração do NavBar para  vue.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,118 @@
+<template>
+    <head>
+    <title>COMEX</title>
+
+    </head>
+    <nav class="navbar navbar-expand-lg">
+    <div class="container">
+      <img src="../assets/logo-1d225b61.svg" class="logo-white" />
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"><i class="fa fa-navicon" style="color:#fff; font-size:30px"></i></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Promoções</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="page/category/category.html">Categorias</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Buscar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Contato</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Carrinho</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Entrar</a>
+          </li>
+        </ul>
+        <form class="search-group disable_mobile">
+          <input class=" input-search" type="search" placeholder="Pesquisar..." aria-label="Search" />
+          <i class="fa-solid fa-cart-shopping"></i>
+          <i class="fa-solid fa-user"></i>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'NavBar',
+})
+</script>
+
+<style>
+
+.navbar {
+    background-color: #8FA8FF;
+    margin-bottom: 1rem;
+    width: 100%;
+}
+
+
+.navbar-expand-lg {
+    display: flex;
+}
+
+
+.nav-link {
+    color: #ffffff; 
+    
+}
+
+.fa-cart-shopping, .fa-user {
+    color: #ffffff;
+}
+
+.search-group {
+    display: flex;
+    align-items: center;
+}
+
+.input-search {
+    background-color: #8FA8FF;
+    border-color: #ffffff;
+    border-width: 0.2rem;
+    border-style: solid;
+    border-radius: 5%;  
+    height: 2rem;
+}
+
+
+.input_container {
+    display: flex;
+    justify-content: center;
+}
+
+.nav-item {
+    margin-right: 5%;
+    
+}
+
+.logo-white {
+    filter: invert(100%) sepia(100%) saturate(15%) hue-rotate(307deg) brightness(106%) contrast(103%);
+}
+
+@media(max-width: 768px) {
+    
+    
+    .input_mobile {
+        border-color: #8FA8FF;
+        border-style: solid;   
+        border-right: 0px;
+        border-width: 0.2rem;
+        width: 80%;
+        margin: auto;  
+        
+    }
+    
+}
+</style>
